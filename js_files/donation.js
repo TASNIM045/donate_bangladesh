2bb31ef7f84af98b22599fc9de4b3dd58ca7402f
tasnim-rahman <tasnimrahman0101@gmail.com>
2025-01-30 12:30:07 +0600

    home page done

--- js_files/donation.js
+++ js_files/donation.js
@@ -7,6 +7,8 @@ document.getElementById('donation-button').addEventListener('click', function ()
     historyArea.classList.add('hidden');
 })
 
+// NOAKHALI DONATION SECTION
+
 document.getElementById('noakhali-flood-donation').addEventListener('click', function () {
     let totalForNoakhali = parseFloat(document.getElementById('noakhali-total-donation').innerText);
     let totalInput = parseFloat(document.getElementById('noakhali-donation-input').value);
@@ -16,10 +18,54 @@ document.getElementById('noakhali-flood-donation').addEventListener('click', fun
         if (myBalance >= totalInput) {
             let totalBalance = totalForNoakhali + totalInput;
             document.getElementById('noakhali-total-donation').innerText = totalBalance;
-            
+
             let newBalance = myBalance - totalInput;
             document.getElementById('my-balance').innerText = newBalance;
             document.getElementById('noakhali-donation-input').value = '';
+        }
+        else {
+            alert("You do not have enuogh money!");
+        }
+    }
+})
+
+// FENI DONATION SECTION 
+
+document.getElementById('feni-flood-donation').addEventListener('click', function () {
+    let totalForFeni = parseFloat(document.getElementById('feni-total-donation').innerText);
+    let totalInput = parseFloat(document.getElementById('feni-donation-input').value);
+
+    if (totalInput > 0 && isNaN(totalInput) == false) {
+        let myBalance = parseFloat(document.getElementById('my-balance').innerText);
+        if (myBalance >= totalInput) {
+            let totalBalance = totalForFeni + totalInput;
+            document.getElementById('feni-total-donation').innerText = totalBalance;
+
+            let newBalance = myBalance - totalInput;
+            document.getElementById('my-balance').innerText = newBalance;
+            document.getElementById('feni-donation-input').value = '';
+        }
+        else {
+            alert("You do not have enuogh money!");
+        }
+    }
+})
+
+// QUOTA DONATION SECTION 
+
+document.getElementById('quota-donation').addEventListener('click', function () {
+    let totalForQuota = parseFloat(document.getElementById('quota-total-donation').innerText);
+    let totalInput = parseFloat(document.getElementById('quota-donation-input').value);
+
+    if (totalInput > 0 && isNaN(totalInput) == false) {
+        let myBalance = parseFloat(document.getElementById('my-balance').innerText);
+        if (myBalance >= totalInput) {
+            let totalBalance = totalForQuota + totalInput;
+            document.getElementById('quota-total-donation').innerText = totalBalance;
+
+            let newBalance = myBalance - totalInput;
+            document.getElementById('my-balance').innerText = newBalance;
+            document.getElementById('quota-donation-input').value = '';
         }
         else {
             alert("You do not have enuogh money!");
