5263bc1e9fadc34a663d899bc66a94ca8f5b45cc
tasnim-rahman <tasnimrahman0101@gmail.com>
2025-01-30 14:39:05 +0600

    finished

--- js_files/blog.js
+++ js_files/blog.js
@@ -1,3 +1,13 @@
 document.getElementById('blog-button').addEventListener('click',function(){
-    console.log('blog button clicked');
+    document.getElementById('main-section').classList.add('hidden');
+    document.getElementById('main-extra').classList.remove('hidden');
+    document.getElementById('blog-button').classList.add('hidden');
+    document.getElementById('home-button').classList.remove('hidden');
+})
+
+document.getElementById('home-button').addEventListener('click',function(){
+    document.getElementById('main-section').classList.remove('hidden');
+    document.getElementById('main-extra').classList.add('hidden');
+    document.getElementById('blog-button').classList.remove('hidden');
+    document.getElementById('home-button').classList.add('hidden');
 })
--- js_files/history.js
+++ js_files/history.js
@@ -7,4 +7,47 @@ document.getElementById('history-button').addEventListener('click', function () 
     historyArea.classList.remove('hidden');
 })
 
+document.getElementById('noakhali-flood-donation').addEventListener('click', function () {
 
+    let totalInput = parseFloat(document.getElementById('noakhali-donation-input').value);
+
+    if (totalInput > 0 && isNaN(totalInput) == false) {
+        document.getElementById('history-area').innerHTML += `
+        <div class="border border-gray-200 rounded-lg">
+        <div class="p-6 space-y-3">
+        <h1 class="text-lg lg:text-xl font-bold">${totalInput} Taka is Donated for Flood Relief in Noakhali,Bangladesh</h1>
+        </div>
+        </div>
+        `
+    }
+})
+
+document.getElementById('feni-flood-donation').addEventListener('click', function () {
+
+    let totalInput = parseFloat(document.getElementById('feni-donation-input').value);
+
+    if (totalInput > 0 && isNaN(totalInput) == false) {
+        document.getElementById('history-area').innerHTML += `
+        <div class="border border-gray-200 rounded-lg">
+        <div class="p-6 space-y-3">
+        <h1 class="text-lg lg:text-xl font-bold">${totalInput} Taka is Donated for Flood Relief in Feni,Bangladesh</h1>
+        </div>
+        </div>
+        `
+    }
+})
+
+document.getElementById('quota-donation').addEventListener('click', function () {
+
+    let totalInput = parseFloat(document.getElementById('quota-donation-input').value);
+
+    if (totalInput > 0 && isNaN(totalInput) == false) {
+        document.getElementById('history-area').innerHTML += `
+        <div class="border border-gray-200 rounded-lg">
+        <div class="p-6 space-y-3">
+        <h1 class="text-lg lg:text-xl font-bold">${totalInput} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</h1>
+        </div>
+        </div>
+        `
+    }
+})
